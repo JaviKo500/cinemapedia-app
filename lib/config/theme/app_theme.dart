e59e446373dc346e7b581d9109642efd0c28252b
JaViKo500 <javikogutierrez64@gmail.com>
2024-06-08 18:23:56 -0500

    movie mapper

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 class AppTheme {
   
   ThemeData getTheme() => ThemeData(
+    scaffoldBackgroundColor: Colors.blueGrey,
     useMaterial3: true,
     colorSchemeSeed: const Color(0xff2862F5),
   );
---
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -0,0 +1,25 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/models/movie_db/movie_db.dart';
+
+class MovieMapper {
+  static Movie movieDbToEntity(MovieMovieDB movieDb) => Movie(
+      adult: movieDb.adult,
+      backdropPath: movieDb.backdropPath.isNotEmpty 
+        ? 'https://image.tmdb.org/t/p/w500${movieDb.backdropPath}'
+        : 'https://static.vecteezy.com/system/resources/previews/008/382/505/original/illustration-of-astronaut-in-space-for-404-website-error-page-not-found-text-cute-template-with-planet-stars-for-poster-banner-or-website-page-vector.jpg',
+      genreIds: movieDb.genreIds.map((e) => e.toString()).toList(),
+      id: movieDb.id,
+      originalLanguage: movieDb.originalLanguage,
+      originalTitle: movieDb.originalTitle,
+      overview: movieDb.overview,
+      popularity: movieDb.popularity,
+      posterPath: movieDb.posterPath.isNotEmpty 
+        ? 'https://image.tmdb.org/t/p/w500${movieDb.posterPath}'
+        : 'https://static.vecteezy.com/system/resources/previews/008/382/505/original/illustration-of-astronaut-in-space-for-404-website-error-page-not-found-text-cute-template-with-planet-stars-for-poster-banner-or-website-page-vector.jpg',
+      releaseDate: movieDb.releaseDate,
+      title: movieDb.title,
+      video: movieDb.video,
+      voteAverage: movieDb.voteAverage,
+      voteCount: movieDb.voteCount
+    );
+}
