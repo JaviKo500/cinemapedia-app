ee79cd0e66d069d1252b85c58ae951a8dde68781
JaViKo500 <javikogutierrez64@gmail.com>
2024-06-29 15:11:54 -0500

    implements movie info provider

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
 
 
-class MovieScreen extends StatelessWidget {
+class MovieScreen extends StatefulWidget {
   static const String name = 'movie-screen';
   final String movieId;
   const MovieScreen ({super.key, required this.movieId});
+
+  @override
+  State<MovieScreen> createState() => _MovieScreenState();
+}
+
+class _MovieScreenState extends State<MovieScreen> {
+  
+  @override
+  void initState() {
+    super.initState();
+    
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          'Movie Screen $movieId',
+          'Movie Screen ${widget.movieId}',
           style: const TextStyle()
         ),
       ),
