ba918f7c11c6f2b3f346f28b1bb4d8067a983e7d
JaViKo500 <javikogutierrez64@gmail.com>
2024-07-08 23:15:52 -0500

    actors list

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,4 +1,6 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:cinemapedia/presentation/providers/movies/movie_info_provider.dart';
+import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -20,6 +22,7 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
   void initState() {
     super.initState();
     ref.read( movieInfoProvider.notifier ).loadMovies( widget.movieId );
+    ref.read( actorsByMovieProvider.notifier ).loadActors( widget.movieId );
   }
 
   @override
@@ -102,9 +105,69 @@ class _MovieDetails extends StatelessWidget {
             ],
           ),
         ),
-        // TODO: show actors
-        const SizedBox(height: 100,),
+        _ActorsByMovie(movieId: movie.id.toString()),
+        const SizedBox(height: 50,),
       ],
+    );
+  }
+}
+
+class _ActorsByMovie extends ConsumerWidget {
+  final String movieId;
+  const _ActorsByMovie({required this.movieId});
+
+  @override
+  Widget build(BuildContext context, ref ) {
+    final actorsByMovie = ref.watch( actorsByMovieProvider );
+    if ( actorsByMovie[movieId] == null) {
+      return const CircularProgressIndicator( strokeWidth: 2, );
+    }
+
+    final actors = actorsByMovie[ movieId ] ?? [];
+
+    return SizedBox(
+      height: 300,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: actors.length,
+        itemBuilder: (context, index) {
+          final actor = actors[index];
+          return FadeInDown(
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              width: 135,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular( 20 ),
+                    child: Image.network(
+                      actor.profilePath,
+                      height: 180,
+                      width: 135,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  const SizedBox(height: 10,),
+                  Text(
+                    actor.name,
+                    style: const TextStyle(),
+                    maxLines: 2,
+                  ),
+                  Text(
+                    actor.character ?? '',
+                    maxLines: 2,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      overflow: TextOverflow.ellipsis
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
