c7829a15a52e1f83db56dae9fc14f2e05402b91c
JaViKo500 <javikogutierrez64@gmail.com>
2024-07-11 22:22:54 -0500

    redirect movie

--- lib/presentation/widgets/shared/custom_app_bar.dart
+++ lib/presentation/widgets/shared/custom_app_bar.dart
@@ -1,7 +1,9 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:cinemapedia/presentation/delegates/search_movie_delegate.dart';
 import 'package:cinemapedia/presentation/providers/movies/movies_repository_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomAppBar extends ConsumerWidget {
   const CustomAppBar({super.key});
@@ -28,12 +30,16 @@ class CustomAppBar extends ConsumerWidget {
               IconButton(
                 onPressed: () {
                   final  movieRepository = ref.read( movieRepositoryProvider );
-                  showSearch(
+                  showSearch<Movie?>(
                     context: context, 
                     delegate: SearchMovieDelegate(
                       searchMovies: movieRepository.searchMovies
                     )
-                  );
+                  ).then((movie) {
+                    if ( movie != null ) {
+                      context.push('/movie/${movie.id}');
+                    }
+                  });
                 },
                 icon: const Icon(Icons.search),
               )
