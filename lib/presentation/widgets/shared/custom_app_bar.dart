41cef6b56c67f93ea24ed61afaaaa32a4960f6a4
JaViKo500 <javikogutierrez64@gmail.com>
2024-07-13 17:33:18 -0500

    search movie query

--- lib/presentation/widgets/shared/custom_app_bar.dart
+++ lib/presentation/widgets/shared/custom_app_bar.dart
@@ -1,6 +1,7 @@
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:cinemapedia/presentation/delegates/search_movie_delegate.dart';
 import 'package:cinemapedia/presentation/providers/movies/movies_repository_provider.dart';
+import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
@@ -30,10 +31,15 @@ class CustomAppBar extends ConsumerWidget {
               IconButton(
                 onPressed: () {
                   final  movieRepository = ref.read( movieRepositoryProvider );
+                  final  searchQuery = ref.read( searchQueryProvider );
                   showSearch<Movie?>(
+                    query: searchQuery,
                     context: context, 
                     delegate: SearchMovieDelegate(
-                      searchMovies: movieRepository.searchMovies
+                      searchMovies: (query) {
+                        ref.read( searchQueryProvider.notifier ).update((state) => query);
+                        return movieRepository.searchMovies(query);
+                      },
                     )
                   ).then((movie) {
                     if ( movie != null ) {
