924306b9d1b8c51fa086c97df5bdc03d7816a520
JaViKo500 <javikogutierrez64@gmail.com>
2024-07-13 19:17:09 -0500

    navigate routes go router

--- lib/presentation/widgets/shared/custom_bottom_navigation_bar.dart
+++ lib/presentation/widgets/shared/custom_bottom_navigation_bar.dart
@@ -1,11 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 
 class CustomBottomNavigationBar extends StatelessWidget {
   const CustomBottomNavigationBar ({super.key});
+  
+  void onItemTapped( BuildContext context, int index ) {
+    switch (index) {
+      case 0:
+        context.go('/');
+        break;
+      case 1:
+        context.go('/categories');
+        break;
+      case 2:
+        context.go('/favorites');
+        break;
+      default:
+        context.go('/');
+    }
+  }
+
+  int getCurrentIndex( BuildContext context ) {
+    final  String location = GoRouterState.of(context).matchedLocation;
+    switch (location) {
+      case '/':
+        return 0;
+      case '/categories':
+        return 1;
+      case '/favorites':
+        return 2;
+      default:
+        return 0;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
+      onTap: (value) => onItemTapped( context, value ),
+      currentIndex: getCurrentIndex( context ),
       items: const [
         BottomNavigationBarItem(
           icon: Icon(Icons.home_max),
