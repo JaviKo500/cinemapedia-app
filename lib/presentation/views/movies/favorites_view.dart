a2d0466329d5e7d59af6819858c486bb4401d543
JaViKo500 <javikogutierrez64@gmail.com>
2024-07-20 00:23:31 -0500

    load factories movies in init state

--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -1,25 +1,41 @@
+import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 
-class FavoritesView extends StatelessWidget {
+class FavoritesView extends ConsumerStatefulWidget {
 
   const FavoritesView ({super.key});
 
   @override
+  FavoritesViewState createState() => FavoritesViewState();
+}
+
+class FavoritesViewState extends ConsumerState<FavoritesView> {
+
+  @override
+  void initState() {
+    super.initState();
+    ref.read( favoriteMoviesProvider.notifier ).loadNextPage();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final favoritesMovies = ref.watch( favoriteMoviesProvider ).values.toList();
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Favorite views',
-          style: TextStyle()
-        ),
-      ),
-      body: const Center(
-        child: Text(
-          'Favorite views',
-          style: TextStyle()
-        ),
-      ),
+      
+      body: ListView.builder(
+        itemCount: favoritesMovies.length,
+        itemBuilder: (context, index) {
+          final movie = favoritesMovies[index];
+          return ListTile(
+            title: Text(
+              'Movie name: ${movie.title}',
+              style: const TextStyle()
+            ),
+          );
+        },
+      )
     );
   }
 }
