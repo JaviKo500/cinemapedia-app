8f556b85e57d8bae4e23363f7ce373aea8af39da
JaViKo500 <javikogutierrez64@gmail.com>
2024-07-21 10:07:40 -0500

    masonry design

--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -1,4 +1,5 @@
 import 'package:cinemapedia/presentation/providers/providers.dart';
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -24,18 +25,7 @@ class FavoritesViewState extends ConsumerState<FavoritesView> {
     final favoritesMovies = ref.watch( favoriteMoviesProvider ).values.toList();
     return Scaffold(
       
-      body: ListView.builder(
-        itemCount: favoritesMovies.length,
-        itemBuilder: (context, index) {
-          final movie = favoritesMovies[index];
-          return ListTile(
-            title: Text(
-              'Movie name: ${movie.title}',
-              style: const TextStyle()
-            ),
-          );
-        },
-      )
+      body: MovieMasonry(movies: favoritesMovies,)
     );
   }
 }
--- lib/presentation/widgets/widgets.dart
+++ lib/presentation/widgets/widgets.dart
@@ -1,5 +1,7 @@
 export 'shared/custom_app_bar.dart';
 export 'movies/movies_slide_show.dart';
+export 'movies/movie_masonry.dart';
+export 'movies/movie_poster_link.dart';
 export 'movies/movie_horizontal_listview.dart';
 export 'shared/custom_bottom_navigation_bar.dart';
 export 'shared/full_screen_loader.dart';
