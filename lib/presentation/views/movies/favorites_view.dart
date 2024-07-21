f99f0ffbccf7e4666c2a8d81a3d02e7f18cc46d7
JaViKo500 <javikogutierrez64@gmail.com>
2024-07-21 13:33:25 -0500

    empty favorites movies

--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -2,6 +2,7 @@ import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 
 class FavoritesView extends ConsumerStatefulWidget {
@@ -36,6 +37,43 @@ class FavoritesViewState extends ConsumerState<FavoritesView> {
   @override
   Widget build(BuildContext context) {
     final favoritesMovies = ref.watch( favoriteMoviesProvider ).values.toList();
+    if ( favoritesMovies.isEmpty ) {
+      final colors = Theme.of(context).colorScheme;
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(Icons.favorite_outline_sharp, size: 60, color: colors.primary),
+            const SizedBox(height: 10,),
+            Text(
+              'Oh no!',
+              style: TextStyle(
+                fontSize: 30,
+                color: colors.primary
+              )
+            ),
+            const Text(
+              'Not have favorites movies',
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.black45
+              )
+            ),
+            const SizedBox(height: 20,),
+            FilledButton.tonal(
+              onPressed: () {
+                context.go('/home/0');
+              }, 
+              child: const Text(
+                'Start search',
+                style: TextStyle()
+              ),
+            )
+          ],
+        ),
+      );
+    }
     return Scaffold(
       
       body: MovieMasonry(
